a478876927d5a04155303e3b9aeb052c3c96ef11
gebreyes <gebreyesis56@gmail.com>
2022-07-07 19:06:23 +0300

    express framweork

---
+++ express_app.js
@@ -0,0 +1,15 @@
+// how to use express framework in node runtime environment
+
+//response.write() Vs response.readFile() Vs response.send() Vs response.sendFile()
+// about {root:__dirname}
+
+const express = require('express');
+// express() invokes the express module instances
+const app = express();
+app.listen(500)
+
+app.get('/', (req, res) => {
+    res.send('<P> First Express app</P>'); // this send() will automatically set
+    //content type and response statuscode
+    // which means you are not obligated to write status code and content-type 
+});
