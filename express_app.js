f96ac655313d56c3385c0dcd0f029b0757795d05
gebreyes <gebreyesis56@gmail.com>
2022-07-07 19:08:32 +0300

    express framework

--- express_app.js
+++ express_app.js
@@ -10,6 +10,6 @@ app.listen(500)
 
 app.get('/', (req, res) => {
     res.send('<P> First Express app</P>'); // this send() will automatically set
-    //content type and response statuscode
+    //content type and response status code
     // which means you are not obligated to write status code and content-type 
 });
